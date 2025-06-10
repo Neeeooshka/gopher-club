41650706ace7c0cd29fba4a38e0b7c9bde45e907
ukraintsevDV <ukraintsevDV@mos-gaz.ru>
2025-06-10 13:05:24 +0300

    fix cycled imports

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,14 +1,5 @@
 package storage
 
-import (
-	"github.com/Neeeooshka/gopher-club.git/internal/services/users"
-)
-
 type Storage interface {
 	Close() error
-}
-
-type UserRepository interface {
-	AddUser(users.User, string) error
-	GetUserByLogin(string) (users.User, error)
 }
