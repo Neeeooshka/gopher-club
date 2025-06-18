68b4d1be3b1ffede41d7ed426f83292aa1b0fd66
ukraintsevDV <ukraintsevDV@mos-gaz.ru>
2025-06-18 10:53:10 +0300

    add order update function

--- internal/storage/postgres/orders.go
+++ internal/storage/postgres/orders.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"database/sql"
 	"github.com/Neeeooshka/gopher-club/internal/services/orders"
 	"github.com/Neeeooshka/gopher-club/internal/services/users"
 )
@@ -28,9 +29,34 @@ func (l *Postgres) AddOrder(number string, userId int) error {
 	return nil
 }
 
-func (l *Postgres) ListOrders(ctx context.Context, user users.User) ([]orders.Order, error) {
+func (l *Postgres) UpdateOrders(ctx context.Context, orders []orders.Order) error {
 
-	var result []orders.Order
+	stmt, err := l.DB.BeginTx(ctx, nil)
+	defer stmt.Rollback()
+
+	for _, order := range orders {
+		_, err = stmt.ExecContext(ctx, "update gopher_orders set status = $1, accrual = $2 where order_id = $3", order.Status, order.Accrual, order.UserID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return stmt.Commit()
+}
+
+func (l *Postgres) ListOrders(ctx context.Context) ([]orders.Order, error) {
+
+	rows, err := l.DB.QueryContext(ctx, "select * from gopher_orders where status not in ('INVALID', 'PROCESSED')")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	return l.extractOrders(rows)
+}
+
+func (l *Postgres) ListUserOrders(ctx context.Context, user users.User) ([]orders.Order, error) {
 
 	rows, err := l.DB.QueryContext(ctx, "select * from gopher_orders where user_id = $1 order by date_insert desc", user.ID)
 	if err != nil {
@@ -38,6 +64,13 @@ func (l *Postgres) ListOrders(ctx context.Context, user users.User) ([]orders.Or
 	}
 
 	defer rows.Close()
+
+	return l.extractOrders(rows)
+}
+
+func (l *Postgres) extractOrders(rows *sql.Rows) ([]orders.Order, error) {
+
+	var result []orders.Order
 
 	for rows.Next() {
 		var o orders.Order
